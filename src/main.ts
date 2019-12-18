256eb6adba2c97f780453b339ec0b4cf77870090
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-12-18 15:38:54 +1000

    feat(main): remove weather icons

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,11 @@ import { MyApp } from './my-app';
 import { resources } from './resources';
 
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
-import '!style-loader!css-loader!weather-icons/css/weather-icons.css';
 
 Aurelia
     .register(
         StyleConfiguration.cssModulesProcessor(),
-        ...resources
+        resources
     )
     .app(MyApp)
     .start();
