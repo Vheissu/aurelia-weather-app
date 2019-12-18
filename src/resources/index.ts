3c42e453ae643a351a90a3014948467b86e7ef7f
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-12-18 21:13:18 +1000

    chore(resources): remove casting for resources

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -1,6 +1,5 @@
 import { FormatDate } from './value-converters/format-date';
-import { IRegistry } from '@aurelia/kernel';
 
 export const resources = [
     FormatDate
-] as unknown as IRegistry[];
+];
