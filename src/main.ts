77296bd856eb7bbd4a3ada079ff631618ecfef96
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-12-18 11:59:48 +1000

    feat(app): import weather icon styling

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { MyApp } from './my-app';
 import { resources } from './resources';
 
 import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.css';
+import '!style-loader!css-loader!weather-icons/css/weather-icons.css';
 
 Aurelia
     .register(
